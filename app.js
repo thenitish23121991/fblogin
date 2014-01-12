0de9cdb6dfa91dd969b97d9a9e1117670162e781
unknown <nitsh7084@gmail.com>
2014-01-12 23:53:11 +0530

    Added backbone code

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer();
 
 app.get('/',function(req,res){
 res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(8080);
+
+
+app.use(express.static(__dirname+"/assets"));
